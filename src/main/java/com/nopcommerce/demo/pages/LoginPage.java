efb78595b26bcba72edcce4d8ac6e102e63fe461
prmc101 <primecommunity101@gmail.com>
2023-05-06 17:50:22 +0100

    Today's class update

--- src/main/java/com/nopcommerce/demo/pages/LoginPage.java
+++ src/main/java/com/nopcommerce/demo/pages/LoginPage.java
@@ -3,18 +3,37 @@ package com.nopcommerce.demo.pages;
 import com.aventstack.extentreports.Status;
 import com.nopcommerce.demo.customlisteners.CustomListeners;
 import com.nopcommerce.demo.utility.Utility;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by Jay Vaghani
  */
 public class LoginPage extends Utility {
 
-    By welcomeText = By.xpath("//h1[contains(text(),'Welcome, Please Sign In!')]");
+    @CacheLookup
+    @FindBy(xpath = "//h1[contains(text(),'Welcome, Please Sign In!')]")
+    WebElement welcomeText;
+    @CacheLookup
+    @FindBy(id = "Email")
+    WebElement emailField;
+
+    @FindBy(name = "Password")
+    WebElement passwordField;
+
+    @CacheLookup
+    @FindBy(xpath = "//button[contains(text(),'Log in')]")
+    WebElement loginButton;
+
+    @CacheLookup
+    @FindBy(xpath = "//div[@class='message-error validation-summary-errors']")
+    WebElement errorMessage;
+    /*By welcomeText = By.xpath("//h1[contains(text(),'Welcome, Please Sign In!')]");
     By emailField = By.id("Email");
     By passwordField = By.name("Password");
     By loginButton = By.xpath("//button[contains(text(),'Log in')]");
-    By errorMessage = By.xpath("//div[@class='message-error validation-summary-errors']");
+    By errorMessage = By.xpath("//div[@class='message-error validation-summary-errors']");*/
 
     public String getWelcomeText(){
         String message = getTextFromElement(welcomeText);
